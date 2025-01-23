3ab23270c0cf57d4701af44a9a86dda0ebda75ce
Басистов Глеб <basistovga@cash-u.com>
2025-01-23 10:00:01 +0300

    fix

--- api/app.py
+++ api/app.py
@@ -8,13 +8,13 @@ from .routers.quiz_result import router as quiz_result_router
 from .routers.session import router as session_router
 from .routers.games import router as game_router
 
-app = FastAPI(docs_url='/api/docs')
-app.include_router(quiz_router, prefix="/api/quiz", tags=['Quiz'])
-app.include_router(quiz_questions_router, prefix="/api/quiz/questions", tags=['Quiz Questions'])
-app.include_router(quiz_question_answer_router, prefix="/api/quiz/questions/answers", tags=['Quiz Questions Answers'])
-app.include_router(quiz_result_router, prefix="/api/quiz/results", tags=['Quiz Results'])
-app.include_router(session_router, prefix="/api/session", tags=['Session'])
-app.include_router(game_router, prefix="/api/game", tags=['Game'])
+app = FastAPI()
+app.include_router(quiz_router, prefix="/quiz", tags=['Quiz'])
+app.include_router(quiz_questions_router, prefix="/quiz/questions", tags=['Quiz Questions'])
+app.include_router(quiz_question_answer_router, prefix="/quiz/questions/answers", tags=['Quiz Questions Answers'])
+app.include_router(quiz_result_router, prefix="/quiz/results", tags=['Quiz Results'])
+app.include_router(session_router, prefix="/session", tags=['Session'])
+app.include_router(game_router, prefix="/game", tags=['Game'])
 
 origins = [
     "http://localhost",
