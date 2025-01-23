7f5a999ad8cfe8ec96a53b6ba214cfb1ccbeb32c
Басистов Глеб <basistovga@cash-u.com>
2025-01-23 10:10:09 +0300

    fix

--- api/app.py
+++ api/app.py
@@ -21,6 +21,8 @@ origins = [
     "http://localhost:8080",
     "http://0.0.0.0:8080",
     "http://127.0.0.1:8080",
+    "https://quiz.kley.media",
+    "https://api.quiz.kley.media",
 ]
     
 app.add_middleware(
