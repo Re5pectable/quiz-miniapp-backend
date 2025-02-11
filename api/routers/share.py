d6a5ddc69811c4fc3f143da4bd543a62132ac7e3
Басистов Глеб <basistovga@cash-u.com>
2025-02-11 20:53:45 +0300

    fix

--- api/routers/share.py
+++ api/routers/share.py
@@ -6,6 +6,6 @@ router = APIRouter()
 
 @router.get("/{invitation_id}")
 async def get_share_page(
-    invitation_id: UUID
+    invitation_id: str
 ):
     return await Game.get_share(invitation_id)
