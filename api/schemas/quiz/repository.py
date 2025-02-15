4447f9440bd0185f0d6b1c512e594ccc77233993
Басистов Глеб <basistovga@cash-u.com>
2025-02-15 11:09:27 +0300

    fix

--- api/schemas/quiz/repository.py
+++ api/schemas/quiz/repository.py
@@ -5,6 +5,6 @@ from ... import db
 
 async def get_quiz_questions_amount(quiz_id) -> tuple[db.QuizOrm, int]:
     async with db.Session() as session:        
-        stmt = select(func.count()).select_from(db.QuizQuestionOrm).filter_by(quiz_id=quiz_id)
+        stmt = select(func.count()).select_from(db.QuizQuestionOrm).where(db.QuizQuestionOrm.quiz_id==quiz_id)
         q = await session.execute(stmt)
         return q.scalars().first()
